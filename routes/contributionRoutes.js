5a5e4475332867d68401126509d4305a1261ac7d
pritom-dey1 <Pritom1.2.zx@gmail.com>
2025-11-09 21:48:32 +0600

    added middleware to contribute

--- routes/contributionRoutes.js
+++ routes/contributionRoutes.js
@@ -1,5 +1,6 @@
 import express from "express"
 import { addContribution, getContributionsByIssue, getContributionsByUser } from "../controllers/contributionController.js"
+import { verifyToken } from "../middleware/verifyToken.js"
 const router = express.Router()
 
 router.get("/", async (req, res) => {
@@ -12,8 +13,8 @@ router.get("/", async (req, res) => {
   }
 })
 
-router.post("/", addContribution)
-router.get("/issue/:issueId", getContributionsByIssue)
-router.get("/user/:email", getContributionsByUser)
+router.post("/",verifyToken, addContribution)
+router.get("/issue/:issueId",verifyToken, getContributionsByIssue)
+router.get("/user/:email",verifyToken, getContributionsByUser)
 
 export default router
